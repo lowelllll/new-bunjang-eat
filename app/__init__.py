9ee8369c2cb30988dfd774ec7a689b1fd8503a47
lowelllll <yejin2651@naver.com>
2019-08-19 09:24:15 +0900

    feat: index page

--- app/__init__.py
+++ app/__init__.py
@@ -7,3 +7,8 @@ from app.env import env
 app = Flask(__name__)
 
 app.config.from_object(config_env[env])
+
+from app.api.home import home
+from app.api.home import controller
+
+app.register_blueprint(home, url_prefix='/')
--- app/env/__init__.py
+++ app/env/__init__.py
@@ -1,4 +1,4 @@
 import os
 
 # bujang_culture
-env = os.getenv('BC_ENV', 'dev')
+env = os.getenv('BUNCUL_ENV', 'dev')
